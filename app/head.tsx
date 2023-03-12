4871c1b547e1c1f150199f43350b75bc4757c25d
guihrib <gr.miraidevelopment@gmail.com>
2023-03-12 19:54:36 -0300

    new deploy

--- app/head.tsx
+++ app/head.tsx
@@ -1,18 +1,8 @@
 const Head = () => {
   return (
     <>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="author" content="Guilherme Ribeiro" />
-      <meta name="keywords" content="Guilherme Ribeiro, GR, Front-End, Developer, Junior, ADS, Student, 2/3" />
-      <meta name="robots" content="index, follow" />
-      <meta name="language" content="Portuguese" />
-      <meta name="rating" content="general" />
-      <meta name="distribution" content="global" />
-      <meta name="googlebot" content="index, follow" />
-      <meta name="google" content="nositelinkssearchbox" />
-      <link rel="icon" href="https://avatars.githubusercontent.com/u/61317250" type="image/webp">
-      <title>Guilherme Ribeiro (GR)</title>
+      <link rel="shortcut icon" href="images/favicon.ico" />
+      <title>Guilherme Ribeiro</title>
       <meta name="theme-color" content="#d1b48f" />
       <meta name="description" content="🦔 I'm at my best" />
     </>
