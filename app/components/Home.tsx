e42c181ee940a6c073fba5d70c1e087cc33c00ee
guihrib <gr.miraidevelopment@gmail.com>
2023-03-12 20:07:29 -0300

    new deploy

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
   <div className="flex items-center px-7 py-7 md:py-24 md:px-24">
     <div className="flex flex-col space-y-4">
-      <h1 className="font-bold text-6xl text-center justify-center"> Hello, I am <span className="text-blue"> Guilherme Ribeiro and also known as GR! </span>!
+      <h1 className="font-bold text-6xl text-center justify-center"> Hello, I am <span className="text-blue"> Guilherme Ribeiro and also known as GR </span>!
       </h1>
       <p className="text-2xl text-center justify-center font-normal">Front-End 🎈 Developer Junior • ADS Student 2/3</p>
       <Socials />
