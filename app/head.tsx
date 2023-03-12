70a62abe1c485811e8f969a5c030b041de3ef722
guihrib <gr.miraidevelopment@gmail.com>
2023-03-12 20:13:30 -0300

    new deploy

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 const Head = () => {
   return (
     <>
-      <link rel="shortcut icon" href="images/favicon.ico" />
+      <link rel="shortcut icon" href="public/favicon.ico" />
       <title>Guilherme Ribeiro</title>
       <meta name="theme-color" content="#d1b48f" />
       <meta name="description" content="🦔 I'm at my best" />
